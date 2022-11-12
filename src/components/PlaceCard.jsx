0c9d6b0a05fff8685b3fd24d7033f409e1ae809d
presh-031 <akingbulugbe.dev@gmail.com>
2022-11-12 10:17:32 +0100

    chore: now copying products assets

--- src/components/PlaceCard.jsx
+++ src/components/PlaceCard.jsx
@@ -1,17 +1,29 @@
+import star from "../assets/star.svg";
+import heart from "../assets/heart.svg";
 const PlaceCard = () => {
   return (
     <div>
       <div>
         <img src="" alt="" />
+        <div>
+          <img src={heart} alt="heart" />
+        </div>
       </div>
       <p>
-        <span></span>
-        <span></span>
+        <span>Desert king</span>
+        <span>1MBT per night</span>
       </p>
       <p>
-        <span></span>
-        <span></span>
+        <span>2345km away</span>
+        <span>available for 2weeks stay</span>
       </p>
+      <div>
+        <img src={star} alt="star" />
+        <img src={star} alt="star" />
+        <img src={star} alt="star" />
+        <img src={star} alt="star" />
+        <img src={star} alt="star" />
+      </div>
     </div>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,17 @@
+import PlaceCard from "./PlaceCard";
+
 const Products = () => {
   return (
     <div>
       <p>Inspiration for your next adventure</p>
+      <PlaceCard />
+      <PlaceCard />
+      <PlaceCard />
+      <PlaceCard />
+      <PlaceCard />
+      <PlaceCard />
+      <PlaceCard />
+      <PlaceCard />
     </div>
   );
 };
